300f92350e27d1e1e6de29b548170f06ba32f8be
Ameya Dighole <asdighole@gmail.com>
2024-04-13 13:05:25 -0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+
+  const a = [1,2,3]
+  const b = []
+  const c = []
+
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
